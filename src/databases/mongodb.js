6aca2f00e549e404cfab4358af80730984d97974
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2022-01-23 21:16:02 -0300

    feat: update mongodb config

--- src/databases/mongodb.js
+++ src/databases/mongodb.js
@@ -1,15 +1,56 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+
+let connection = null;
 
 class MongoDB {
   static createConnection() {
     const options = {
+      dbName: process.env.MONGO_NAME,
       useNewUrlParser: true,
-      useFindAndModify: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
+      poolSize: 5,
+      serverSelectionTimeoutMS: 15000,
+      socketTimeoutMS: 20000,
+      connectTimeoutMS: 15000,
     };
 
-    return mongoose.connect(`mongodb://${process.env.MONGO_HOST}`, options);
+    return mongoose.createConnection(
+      `mongodb://${process.env.MONGO_HOST}`,
+      options
+    );
+  }
+
+  static startEvents() {
+    connection.on('disconnected', () => {
+      connection = MongoDB.createConnection();
+    });
+
+    connection.on('error', () => mongoose.disconnect());
+
+    process.on('SIGINT', () => {
+      mongoose.disconnect((err) => {
+        process.exit(err ? 1 : 0);
+      });
+    });
+
+    process.on('message', (msg) => {
+      if (msg === 'shutdown') {
+        mongoose.disconnect((err) => {
+          process.exit(err ? 1 : 0);
+        });
+      }
+    });
+  }
+
+  static getOrCreateConnection() {
+    if (!connection) {
+      connection = MongoDB.createConnection();
+      MongoDB.startEvents();
+    }
+
+    return connection;
   }
 }
 
-export default MongoDB;
+module.exports = MongoDB;
